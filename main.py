11783986f7c3ce6fa04fba25730f5146928f3706
adityaguggari <96553801+adityaguggari@users.noreply.github.com>
2023-04-12 17:11:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 shared_secret_key = os.urandom(32)
 
-def encrypt_message(message, key):
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-    encryptor = cipher.encryptor()
-    padded_message = message + (16 - len(message) % 16) * chr(16 - len(message) % 16)
-    ciphertext = encryptor.update(padded_message.encode()) + encryptor.finalize()
-    return iv + ciphertext
+
 
 def decrypt_message(ciphertext, key):
     iv = ciphertext[:16]
