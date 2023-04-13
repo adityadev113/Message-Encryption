4cd5036f2458d55c42493bbcd382bb4ca7469fa5
Samhita123 <62867134+Samhita123@users.noreply.github.com>
2023-04-12 17:07:43 -0700

    Update main.py
    added decrypt function

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def encrypt_message(message, key):
     padded_message = message + (16 - len(message) % 16) * chr(16 - len(message) % 16)
     ciphertext = encryptor.update(padded_message.encode()) + encryptor.finalize()
     return iv + ciphertext
+
+def decrypt_message(ciphertext, key):
+    iv = ciphertext[:16]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    plaintext = decryptor.update(ciphertext[16:]) + decryptor.finalize()
+    padding_length = plaintext[-1]
+    plaintext = plaintext[:-padding_length]
+    return plaintext.decode()
